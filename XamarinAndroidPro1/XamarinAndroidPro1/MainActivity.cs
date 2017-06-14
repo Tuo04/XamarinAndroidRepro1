287baf5523cc7f7596c3720820757ecfeaf8170e
Tuo Chen (Beyondsoft Corporation) <v-tuchen@microsoft.com>
2017-06-14 04:04:12 -0700

    Test

--- XamarinAndroidPro1/XamarinAndroidPro1/MainActivity.cs
+++ XamarinAndroidPro1/XamarinAndroidPro1/MainActivity.cs
@@ -16,7 +16,7 @@ namespace XamarinAndroidPro1
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
-            MobileCenter.Start("9e491e11-07ef-4ad3-94fd-dfc23febe46c",
+            MobileCenter.Start("0ed7132c-4ae8-4701-bab7-6c958ae7d95f",
                    typeof(Analytics), typeof(Crashes));
         }
     }
